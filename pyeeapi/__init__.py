3a74ed1feaf2aada2f9f8b008536dea37cb3dd0d
Jaimedgp <jaime.diez.gp@gmail.com>
2021-12-20 17:10:01 +0100

    version 1.0

--- pyeeapi/__init__.py
+++ pyeeapi/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = "Jaimedgp"
 __author_orcid__ = "0000-0001-7268-9933"
-__date__ = "November, 2021"
+__date__ = "December, 2021"
 
 __license__ = "GPL-3.0 License"
 __version__ = "0.0.1"
@@ -10,6 +10,3 @@ __maintainer__ = "Jaimedgp"
 __email__ = "jaime.diez.gp@gmail.com"
 
 __repository__ = "https://github.com/Jaimedgp/pyEEApi"
-
-from pyeeapi.air_quality import AirQuality
-
---
+++ pyeeapi/eea_request.py
@@ -0,0 +1,7 @@
+""" EEA API MODULE
+-----------------
+
+Python module to operate with EEA Download Portal
+
+:author Jaimedgp
+"""
